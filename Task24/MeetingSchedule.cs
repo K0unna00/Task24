a67e62caa72b09b03c4dc30105c194eac27661da
K0unn00 <78pf4jz@code.edu.az>
2022-04-13 09:55:32 +0400

    Add project files.

---
+++ Task24/MeetingSchedule.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task24
+{
+    internal class MeetingSchedule
+    {
+        public List<Meeting> meetings = new List<Meeting>();
+        public void SetMeeting(string fullName, DateTime from, DateTime to)
+        {
+            Meeting meeting = new Meeting();
+            int count = 0;
+            foreach (var item in meetings)
+            {
+                if (item.FromDate > from || item.ToDate < to) count++;
+            }
+            if(count == meetings.Count)
+            {
+                meeting.ToDate = to;
+                meeting.Name = fullName;
+                meeting.FromDate = from;
+                meetings.Add(meeting);
+            }
+        }
+        public int FindMeetingCount(DateTime dT)
+        {   
+            int count = 0;
+            foreach (var item in meetings)
+            {
+                if (item.FromDate > dT) count++;
+            }
+            return count;
+        }
+        public bool IsExistsMeetingByName(string name)
+        {
+            foreach (var item in meetings)
+            {
+                if(item.Name == name) return true;
+            }
+            return false;
+        }
+        public List<Meeting> GetExistsMeetins(string name)
+        {
+            List<Meeting> newMeetins = new List<Meeting>();
+            foreach (var item in meetings)
+            {
+                if (item.Name.Contains(name)) newMeetins.Add(item);
+            }
+            return newMeetins;
+        }
+    }
+}
---
+++ Task24/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Meeting meeting2 = new Meeting()
+            {
+            FromDate = DateTime.Parse("5.10.2020"),
+            ToDate = DateTime.Parse("7.10.2020"),
+            Name = "salam"
+            }; 
+            MeetingSchedule meetingSchedule = new MeetingSchedule();
+            meetingSchedule.meetings.Add(meeting2);
+            meetingSchedule.SetMeeting("Atakisiyev Bextiyar" , DateTime.Parse("1.14.2022") , DateTime.Parse("3.10.2022"));
+            meetingSchedule.SetMeeting("salam Bextiyar" , DateTime.Parse("5.10.2022") , DateTime.Parse("7.10.2022"));
+            meetingSchedule.SetMeeting("sagol hikmet" , DateTime.Parse("9.10.2022") , DateTime.Parse("11.10.2022"));
+            meetingSchedule.SetMeeting("salam hikmet" , DateTime.Parse("10.10.2022") , DateTime.Parse("10.7.2022"));
+            foreach (var item in meetingSchedule.meetings)
+            {
+                Console.WriteLine(item.Name);
+            }
+            Console.WriteLine(meetingSchedule.FindMeetingCount(DateTime.Parse("1.10.2022")));
+        }
+    }
+}
